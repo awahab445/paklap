53851154408441b758c50d27e47ca4a682b91605
abdul wahab <a.wahab@mage2experts.com>
2018-03-13 02:54:10 +0500

    Price formatting issue on frontend using Price format module

---
+++ app/code/Arpatech/PriceFormat/view/frontend/requirejs-config.js
@@ -0,0 +1,12 @@
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+var config = {
+    map: {
+        "*": {
+            'Magento_Catalog/js/price-utils':  'Arpatech_PriceFormat/js/price-utils'
+        }
+    }
+};
